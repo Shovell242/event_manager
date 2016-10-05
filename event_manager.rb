1ca5dc85cad8134412b9a324fa747eaafe7351f1
Ryan Lovell <ryanlovell@Ryans-MacBook-Pro.local>
2016-10-04 23:01:16 -0400

    Added zipcode function and API call to sunlight

--- event_manager.rb
+++ event_manager.rb
@@ -1,14 +1,29 @@
 require "csv"
+require "sunlight/congress"
 
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.map { |legislator| "#{legislator.first_name} #{legislator.last_name}" }
+
+	legislator_names.join(", ")
+end
 
 event_file = "event_attendees.csv"
 
 contents = CSV.open(event_file, headers: true, header_converters: :symbol) 
 
 contents.each do |row|
-	puts row[:Zipcode]
+	name = row[:first_name]
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
+	puts "#{name} #{zipcode} #{legislators}"
 end
 
 
